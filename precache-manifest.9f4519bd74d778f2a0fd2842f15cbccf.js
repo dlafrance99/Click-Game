d3ce3d26416f54731bb4e569207c7120cdc6c12c
Devon LaFrance <lafrance.devon@gmail.com>
2019-11-06 14:31:38 -0700

    Updates

---
+++ precache-manifest.9f4519bd74d778f2a0fd2842f15cbccf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c11410a3d0fb3d7ecbeb32b25720d5d2",
+    "url": "/Click-Game/index.html"
+  },
+  {
+    "revision": "7842763d3f941041043a",
+    "url": "/Click-Game/static/css/main.51f694e2.chunk.css"
+  },
+  {
+    "revision": "452f6e77e7a5447fbe56",
+    "url": "/Click-Game/static/js/2.bf6c4cd5.chunk.js"
+  },
+  {
+    "revision": "7842763d3f941041043a",
+    "url": "/Click-Game/static/js/main.98944ad3.chunk.js"
+  },
+  {
+    "revision": "a5c615a5eae15ac288f9",
+    "url": "/Click-Game/static/js/runtime-main.96944184.js"
+  }
+]);
